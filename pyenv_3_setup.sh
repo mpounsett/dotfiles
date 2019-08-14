cbd3d0f44fee812c2b453fe706351e85981c3988
Matthew Pounsett <matt@conundrum.com>
2019-08-14 16:45:36 -0400

    using current python version name in py3 virtualenvs

--- pyenv_3_setup.sh
+++ pyenv_3_setup.sh
@@ -6,19 +6,19 @@ PY2VER=2.7.16
 PY3VER=3.7.4
 
 # set up pyenv environments
-pyenv virtualenv ${PY3VER} jupyter3
-pyenv virtualenv ${PY3VER} tools3
+pyenv virtualenv ${PY3VER} jupyter${PY3VER}
+pyenv virtualenv ${PY3VER} tools${PY3VER}
 
 # set up Jupiter3 environment
-pyenv activate jupyter3
+pyenv activate jupyter${PY3VER}
 pip install jupyter
 python -m ipykernel install --user
 pyenv deactivate
 
 # set up tools3 environment
-pyenv activate tools3
+pyenv activate tools${PY3VER}
 pip install flake8 pdf-diff3 powerline-status restview xml2rfc
 pyenv deactivate
 
 # set the global pyenv shim path
-pyenv global ${PY3VER} ${PY2VER} jupyter3 ipython2 tools3 tools2
+pyenv global ${PY3VER} ${PY2VER} jupyter${PY3VER} ipython2 tools${PY3VER} tools2
