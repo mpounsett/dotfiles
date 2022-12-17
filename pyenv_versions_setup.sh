7cc7a26162cc872ef5d2d6890f1953d214c1d372
Matthew Pounsett <matt@conundrum.com>
2022-12-17 10:57:45 -0500

    updating pyenv versions

--- pyenv_versions_setup.sh
+++ pyenv_versions_setup.sh
@@ -8,10 +8,10 @@ export PATH=${PYENV_ROOT}/bin:${PATH}
 eval "$(pyenv init -)"
 
 PY2VER=(2.7.18)
-PY3VER=(3.10.7 3.9.9 3.8.12 3.7.12 3.6.15)
+PY3VER=(3.10.9 3.9.16 3.8.16 3.7.16 3.6.15)
 
 PY2_CURRENT=2.7.18
-PY3_CURRENT=3.10.7
+PY3_CURRENT=3.10.9
 
 for VER in $PY2VER $PY3VER; do
 	echo "========================"
@@ -25,16 +25,16 @@ pyenv virtualenv ${PY3_CURRENT} jupyter${PY3_CURRENT}
 pyenv virtualenv ${PY3_CURRENT} tools${PY3_CURRENT}
 
 # set up Jupiter3 environment
-pyenv activate jupyter${PY3_CURRENT}
-pip install jupyter ipython notebook
-python -m ipykernel install --user \
-	--name jupyter${PY3_CURRENT} --display-name "Python ${PY3_CURRENT}"
-pyenv deactivate
+# pyenv activate jupyter${PY3_CURRENT}
+# pip install jupyter ipython notebook
+# python -m ipykernel install --user \
+	# --name jupyter${PY3_CURRENT} --display-name "Python ${PY3_CURRENT}"
+# pyenv deactivate
 
 # set up tools3 environment
-pyenv activate tools${PY3_CURRENT}
-pip install flake8 pdf-diff3 powerline-status restview xml2rfc linkchecker
-pyenv deactivate
+# pyenv activate tools${PY3_CURRENT}
+# pip install flake8 pdf-diff3 powerline-status restview xml2rfc linkchecker
+# pyenv deactivate
 
 # set the global pyenv shim path
 pyenv global ${PY3VER} ${PY2VER} \
