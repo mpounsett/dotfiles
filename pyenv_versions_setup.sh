149769511964a526b11dbeaba724d9e97cf2e288
Matthew Pounsett <matt@conundrum.com>
2025-05-21 21:09:23 -0400

    fix script to properly set up tools environments

--- pyenv_versions_setup.sh
+++ pyenv_versions_setup.sh
@@ -12,7 +12,7 @@ PY3VER=(3.13.3 3.12.10 3.11.12 3.10.17 3.9.22 3.8.20 3.7.17 3.6.15)
 
 PY2_CURRENT=2.7.18
 PY3_CURRENT=3.13.3
-PY3_TOOLS=3.11.11
+PY3_TOOLS=3.13.3
 
 for VER in $PY2VER $PY3VER; do
 	echo "========================"
@@ -26,16 +26,17 @@ pyenv virtualenv ${PY3_TOOLS} jupyter${PY3_TOOLS}
 pyenv virtualenv ${PY3_TOOLS} tools${PY3_TOOLS}
 
 # set up Jupiter3 environment
-# pyenv activate jupyter${PY3_TOOLS}
-# pip install jupyter ipython notebook
-# python -m ipykernel install --user \
-	# --name jupyter${PY3_TOOLS} --display-name "Python ${PY3_TOOLS}"
-# pyenv deactivate
+pyenv shell jupyter${PY3_TOOLS} || exit "Failed to open jupyter shell"
+pip install jupyter ipython notebook
+python -m ipykernel install --user \
+	--name jupyter${PY3_TOOLS} --display-name "Python ${PY3_TOOLS}"
+pyenv shell --unset
 
 # set up tools3 environment
-# pyenv activate tools${PY3_TOOLS}
-# pip install flake8 pdf-diff3 powerline-status restview rst2pdf xml2rfc linkchecker
-# pyenv deactivate
+pyenv shell tools${PY3_TOOLS} || exit "Failed to open tools shell"
+pip install flake8 powerline-status restview rst2pdf xml2rfc \
+	linkchecker
+pyenv shell --unset
 
 # set the global pyenv shim path
 pyenv global ${PY3VER} ${PY2VER} \
