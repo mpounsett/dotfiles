e6b782f218df0bee8f2ec449eabf2b3e61aa3271
Matthew Pounsett <matt@conundrum.com>
2025-10-11 18:17:07 -0400

    setting sane indentation in pyenv_update.sh

--- pyenv_update.sh
+++ pyenv_update.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+# vim:autoindent:expandtab:sw=2:ts=2
 
 if [[ -x "${HOME}/.pyenv/bin/pyenv" ]]; then
-	PYENV_ROOT=~/.pyenv
-	for dir in 	${PYENV_ROOT} \
-				${PYENV_ROOT}/plugins/pyenv-virtualenv\
-				${PYENV_ROOT}/plugins/pyenv-virtualenvwrapper; do
-		cd ${dir}
-		git pull --rebase
-	done
+  PYENV_ROOT=~/.pyenv
+  for dir in  ${PYENV_ROOT} \
+    ${PYENV_ROOT}/plugins/pyenv-virtualenv\
+    ${PYENV_ROOT}/plugins/pyenv-virtualenvwrapper; do
+      cd ${dir}
+      git pull --rebase
+  done
 fi
